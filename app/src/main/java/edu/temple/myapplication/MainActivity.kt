65f94d37e24cc1149b6ba6963793b9c3d537bb8d
iiMadina <misrailova11@gmail.com>
2024-11-14 12:13:23 -0500

    added handler

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -5,24 +5,35 @@ import android.content.Intent
 import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var timerBinder: TimerService.TimerBinder
     private var isConnected = false
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val startButton = findViewById<Button>(R.id.startButton)
-        val counter = 0
+        var counter = 0
+        textView = findViewById(R.id.textView)
+
+        val timerHandler = Handler(Looper.getMainLooper()) {
+            textView.text = it.what.toString()
+            true
+        }
 
         val serviceIntent = Intent(this, TimerService::class.java)
 
         val serviceConnection = object : ServiceConnection {
             override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
                 timerBinder = service as TimerService.TimerBinder
+                timerBinder.setHandler(timerHandler)
                 isConnected = true
             }
 
@@ -34,12 +45,17 @@ class MainActivity : AppCompatActivity() {
         bindService(serviceIntent, serviceConnection, BIND_AUTO_CREATE)
 
         startButton.setOnClickListener {
-            if (counter % 2 == 0)
-                startButton.text = "Pause"
-            else
-                startButton.text = "Resume"
-            startService(serviceIntent)
+            if (isConnected) {
+                if (counter % 2 == 0) {
+                    startButton.text = "Pause"
+                    timerBinder.start(100)
+                    counter++
 
+                } else {
+                    startButton.text = "Resume"
+                    counter++
+                }
+            }
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
